1961b368d19f7f408c37ea52af71f7b9965c215a
Morgan-Mischo <goramorgan@gmail.com>
2019-06-14 16:04:08 -0600

    Finished

--- classes.js
+++ classes.js
@@ -29,7 +29,19 @@
   Call your class Employee and receive all the data in the constructor in the order listed above.
 */
 
-//Code Here
+class Employee {
+  constructor(first_name, last_name, email, age)
+  {
+    this.first_name = first_name; 
+    this.last_name = last_name; 
+    this.email = email; 
+    this.age = age; 
+  }
+  makeWidget()
+  {
+    return `${this.first_name} ${this.last_name} Widget`; 
+  }
+}
 
 
 ////////// PROBLEM 2 //////////
@@ -47,7 +59,23 @@
   Call your new class Manager
 */
 
-//Code Here
+class Manager extends Employee{
+constructor(first_name, last_name, email, age, reports)
+{
+  super(first_name, last_name, email, age); 
+  this.reports = []; 
+}
+
+hire(employee)
+{
+  this.reports.push(employee); 
+}
+
+fire(index)
+{
+  this.reports.splice(index, 1); 
+}
+}
 
 
 ////////// PROBLEM 3 //////////
@@ -71,8 +99,40 @@
   Call your new class ProgressiveManager
 */
 
-//Code Here
-
+class ProgressiveManager extends Manager{
+  constructor(first_name, last_name, email, age, reports, bonus, title) {
+    super(first_name, last_name, email, age, reports)
+    this.bonus = 0;
+    this.title = 'Not a manager'
+  }
+ 
+  hire(employee){
+    super.hire(employee);
+    this.updateM()
+    }
+  fire(index){
+    super.fire(index);
+    this.updateM()
+    return this.bonus += 100
+  }
+ 
+  updateM() {
+      if(this.reports.length > 100) {
+        return this.title = 'Bestest Manager';
+    }else if(this.reports.length > 50) {
+        return this.title = 'Manager Plus';
+    }else if(this.reports.length > 10){
+        return this.title = 'Manager';
+    }else if(this.reports.length > 3) {
+        return this.title = 'Mostly Manager';
+    }else if(this.reports.length > 0) {
+        return this.title = 'Barely Manager';
+    } else if(this.reports.length == 0)
+        return this.title = 'Not a Manager'
+ 
+    }
+ 
+  }
 
 
 ////////// PROBLEM 4 - Black Diamond //////////
